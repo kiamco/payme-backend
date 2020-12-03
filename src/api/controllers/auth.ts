a822149bee8a3b9f09d8fbc548730b118024b0ef
Kim Kiamco <kim.kiamco@menlosecurity.com>
2020-12-02 16:41:15 -0800

    able to create and provision tests balances

--- src/api/controllers/auth.ts
+++ src/api/controllers/auth.ts
@@ -1,8 +1,18 @@
-import Express, {Request, Response} from 'express';
+import Express, {Request, Response, request} from 'express';
 import Bcrypt from 'bcryptjs';
-// import jwt from 'jsonwebtoken';
 import User from '../models/userModel';
-import { Keypair } from 'stellar-sdk';
+import { Keypair, Networks, Operation, Server, TransactionBuilder } from 'stellar-sdk';
+import Axios from 'axios';
+import { AES, enc} from 'crypto-js';
+
+const provisionFundsTestNet = async (keypair: any) => {
+    try{
+        const res = await Axios.get(`https://friendbot.stellar.org?addr=${keypair.publicKey()}`);
+    } catch(e) {
+        console.error(e);
+    };
+};
+
 
 const createUser = async ( req: Request, res: Response) => {
 
@@ -15,8 +25,11 @@ const createUser = async ( req: Request, res: Response) => {
     //generate stellar credentials
     const keypair = Keypair.random();
 
-    // has stellar secret 
-    const stellarSecretHash = Bcrypt.hashSync(keypair.secret(),10);
+    // encrypt stellar secret
+    const stellarSecretHash = AES.encrypt(
+        keypair.secret(),
+        password
+    ).toString();
 
     // create user object to insert to monogodb
     const user = new User({
@@ -29,6 +42,7 @@ const createUser = async ( req: Request, res: Response) => {
 
     try {
         const newUser = await user.save();
+        provisionFundsTestNet(keypair);
 
         res.status(201).json({
             message: "User created",
@@ -39,11 +53,73 @@ const createUser = async ( req: Request, res: Response) => {
         res.status(500).json({
             err: e,
             message: 'server error'
-        })
-    }
+        });
+    };
 
+};
+
+const login = async (req: Request,res: Response) => {
+    const {email,token, name, stellarAccount} = req.body;
+
+    try {   
+
+        const account = await checkBalance(stellarAccount);
+        
+        res.status(201).json({
+            message:'user logged in',
+            data: {
+                email,
+                token,
+                name, 
+                stellarAccount,
+                accountInfo: account.balances
+            }
+        });
+    } catch(e) {
+        res.status(500).json({
+            message:'server error',
+            err:e
+        });
+    };
+};
+
+const decryptSecret = async(req:Request, res:Response) => {
+    const {email,password} = req.body;
+
+    try{
+        //get user with find 
+        const user:any = await User.find({
+            email:email
+        });
+
+        // decrypt secret
+        const stellarSecret:any = AES.decrypt(user[0].stellarSeed,password);
+        
+        res.status(201).json({
+            message:'decrypted secret',
+            stellarSecret: stellarSecret.toString(enc.Utf8)
+        });
+
+    } catch(e) {
+        res.status(500).json({
+            message:'server error',
+            err:e 
+        });
+    };
+    
+
+};
+
+const checkBalance = async (publicKey: any) => {
+    const server = new Server('https://horizon-testnet.stellar.org');
+
+    const account = await server.loadAccount(publicKey);
+
+    return account;
 }
 
 export {
-    createUser
-}
+    createUser,
+    login,
+    decryptSecret
+};
